c08463544fb150d098b30e21e43503ba6bbbf6f3
Deepatharani04 <deepatharanigovindaraj@gmail.com>
2024-08-11 09:39:26 +0530

    Add files via upload

---
+++ Task2/App.js
@@ -0,0 +1,12 @@
+
+import Task2 from "./Components/Task2";
+import './style2.css';
+
+
+function App() {
+  return (
+    <Task2 />
+  );
+}
+
+export default App;
---
+++ Task2/Task2.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Task2(){
+    return(
+        <>
+        <div className="task2">
+            <h1 className="text">Social Buttons</h1>
+            <button className="like">Like</button>
+            <button className="comment">Comment</button>
+            <button className="share">Share</button>
+        </div>
+        </>
+    );
+}
+
+export default Task2;
